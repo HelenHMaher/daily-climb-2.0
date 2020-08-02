e559d2720416a17cd87c3b4a1df4636260f895d3
HelenHMaher <helenharlanmaher@gmail.com>
2020-08-02 09:46:41 +0200

    read only files

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+const mongo = require("mongodb").MongoClient;
 
 app.use(
   session({
@@ -13,20 +14,35 @@ app.use(
   })
 );
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+mongo.connect(
+  process.env.MONGO_URI,
+  { useNewURLParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    let db = client.db("my-daily-climb");
+    if (err) {
+      console.log("Database err: " + err);
+    } else {
+      console.group("Successful database connection");
 
-app.use(favicon(__dirname + "/build/favicon.ico"));
-app.use("/", express.static(path.join(__dirname, "build")));
-app.get("/heartbeat", function (req, res) {
-  res.send("<3");
-});
-app.get("/login", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "login.html"));
-});
+      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+      app.use(favicon(__dirname + "/build/favicon.ico"));
+      app.use("/", express.static(path.join(__dirname, "build")));
+      app.get("/heartbeat", function (req, res) {
+        res.send("<3");
+      });
+      app.get("/login", function (req, res) {
+        res.sendFile(path.join(__dirname, "build", "login.html"));
+      });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+      app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "build", "index.html"));
+      });
+
+      app.listen(process.env.PORT || 3000, () =>
+        console.log("Server is running...")
+      );
+    }
+  }
+);
